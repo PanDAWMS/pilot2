592acd00fcd53626c736e6cb84fe7aff70265e68
Daniel Driezshuk <d.drizhuk@gmail.com>
2017-07-26 22:00:39 +0200

    More magics

--- pilot/util/config.py
+++ pilot/util/config.py
@@ -11,6 +11,10 @@
 import ConfigParser
 import os
 import re
+import collections
+
+
+_section_internal = collections.namedtuple('config', 'conf name')
 
 _default_cfg = os.path.join(os.path.dirname(__file__), 'default.cfg')
 _locations = [
@@ -21,27 +25,52 @@ _locations = [
 
 
 class Section(object):
-    config = None
-    name = None
+    int = None
 
     def __init__(self, _config, name):
-        object.__setattr__(self, 'config', _config)
-        object.__setattr__(self, 'name', name)
+        object.__setattr__(self, 'int', _section_internal(_config, name))
 
     def __getitem__(self, item):
-        return object.__getattribute__(self, 'config').get(object.__getattribute__(self, 'name'), item, None)
+        i = object.__getattribute__(self, 'int')
+        return i.conf.get(i.name, item, None)
 
     def __getattr__(self, item):
-        return object.__getattribute__(self, 'config').get(object.__getattribute__(self, 'name'), item, None)
+        i = object.__getattribute__(self, 'int')
+        return i.conf.get(i.name, item, None)
+
+    def __contains__(self, key):
+        i = object.__getattribute__(self, 'int')
+        return i.conf.has_option(i.name, key)
+
+    def __dir__(self):
+        i = object.__getattribute__(self, 'int')
+        return i.conf.options(i.name)
+
+    def __iter__(self):
+        i = object.__getattribute__(self, 'int')
+        return i.conf.options(i.name).__iter__()
 
     def __setitem__(self, key, value):
-        object.__getattribute__(self, 'config').set(object.__getattribute__(self, 'name'), key, value)
+        i = object.__getattribute__(self, 'int')
+        i.conf.set(i.conf.name, key, value)
 
     def __setattr__(self, key, value):
-        object.__getattribute__(self, 'config').set(object.__getattribute__(self, 'name'), key, value)
+        i = object.__getattribute__(self, 'int')
+        i.conf.set(i.conf.name, key, value)
+
+    def __delattr__(self, item):
+        i = object.__getattribute__(self, 'int')
+        if i.conf.has_option(i.name, item):
+            i.conf.remove_option(i.name, item)
+
+    def __delitem__(self, item):
+        i = object.__getattribute__(self, 'int')
+        if i.conf.has_option(i.name, item):
+            i.conf.remove_option(i.name, item)
 
     def get(self, *arg):
-        return object.__getattribute__(self, 'config').get(object.__getattribute__(self, 'name'), *arg)
+        i = object.__getattribute__(self, 'int')
+        return i.conf.get(i.conf.name, *arg)
 
 
 class ExtendedConfig(ConfigParser.ConfigParser):
@@ -60,6 +89,59 @@ class ExtendedConfig(ConfigParser.ConfigParser):
             if self.has_section(item):
                 return Section(self, item)
             raise
+
+    def __iter__(self):
+        return self.sections().__iter__()
+
+    def __dir__(self):
+        def get_attrs(obj):
+            import types
+            if not hasattr(obj, '__dict__'):
+                return []  # slots only
+            if not isinstance(obj.__dict__, (dict, types.DictProxyType)):
+                raise TypeError("%s.__dict__ is not a dictionary"
+                                "" % obj.__name__)
+            return obj.__dict__.keys()
+
+        def dir2(obj):
+            _dir = set()
+            if not hasattr(obj, '__bases__'):
+                # obj is an instance
+                if not hasattr(obj, '__class__'):
+                    # slots
+                    return sorted(get_attrs(obj))
+                _class = obj.__class__
+                _dir.update(get_attrs(_class))
+            else:
+                # obj is a class
+                _class = obj
+
+            for cls in _class.__bases__:
+                _dir.update(get_attrs(cls))
+                _dir.update(dir2(cls))
+            _dir.update(get_attrs(obj))
+            return _dir
+
+        _dir = set(dir2(self))
+        _dir.update(self.sections())
+
+        return list(_dir)
+
+    def __contains__(self, key):
+        return self.has_section(key)
+
+    def __delattr__(self, item):
+        try:
+            return object.__getattribute__(self, item)
+        except AttributeError:
+            if self.has_section(item):
+                self.remove_section(item)
+            else:
+                object.__delattr__(self, item)
+
+    def __delitem__(self, item):
+        if self.has_section(item):
+            self.remove_section(item)
 
 SYMBOLS = {
     'customary': ('B', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'),
