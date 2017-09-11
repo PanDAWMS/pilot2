c6317d6271317cd4d0406d7f1010dbd60dd7ce0b
PalNilsson <paul.nilsson@cern.ch>
2017-09-11 15:35:55 +0200

    First commit, two new functions

---
+++ pilot/util/workernode.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Paul Nilsson, paul.nilsson@cern.ch
+
+from pilot.util.disk import disk_usage
+from pilot.util.parameters import get_maximum_input_sizes
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+# UNIT TEST MISSING (should return non-zero float, float)
+def collect_WN_info():
+    """
+    Collect node information (cpu and memory).
+
+    :return: mem (float), cpu (float)
+    """
+
+    mem = 0.
+    cpu = 0.
+
+    try:
+        with open("/proc/meminfo", "r") as fd:
+            mems = fd.readline()
+            while mems:
+                if mems.upper().find("MEMTOTAL") != -1:
+                    mem = float(mems.split()[1]) / 1024
+                    break
+                mems = fd.readline()
+    except IOError as e:
+        logger.warning("failed to read /proc/meminfo: %s" % e)
+
+    try:
+        with open("/proc/cpuinfo", "r") as fd:
+            lines = fd.readlines()
+            for line in lines:
+                if not string.find(line, "cpu MHz"):
+                    cpu = float(line.split(":")[1])
+                    break
+    except IOError as e:
+        logger.warning("failed to read /proc/cpuinfo: %s" % e)
+
+    return mem, cpu
+
+
+# UNIT TEST MISSING (should return non-zero int)
+def get_disk_space_for_dispatcher(queuedata):
+    """
+
+    Return the amound of disk space that should be available for running the job, either what is actually locally
+    available or the allowed size determined by the site (value from queuedata). This value is only to be used
+    internally by the job dispatcher.
+
+    :param queuedata: (better to have a file based queuedata a la pilot 1 or some singleton memory resident object?)
+    :return: disk space that should be available for running the job
+    """
+
+    _maxinputsize = get_maximum_input_sizes(queuedata)
+    try:
+        du = disk_usage(os.path.abspath("."))
+        _diskspace = int(du[2]/(1024*1024))  # need to convert from B to MB
+    except ValueError, e:
+        logger.warning("Failed to extract disk space: %s (will use schedconfig default)" % e)
+        _diskspace = _maxinputsize
+    else:
+        logger.info("Available WN disk space: %d MB" % (_diskspace))
+
+    _diskspace = min(_diskspace, _maxinputsize)
+    logger.info("Sending disk space %d MB to dispatcher" % (_diskspace))
+
+    return _diskspace
