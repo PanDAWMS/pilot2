748ad8938b62bd1b5e3a65424d2e191a0c3e5758
PalNilsson <paul.nilsson@cern.ch>
2017-09-14 13:00:50 +0200

    Added unit test for collect_workernode_info()

---
+++ pilot/test/test_utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Paul Nilsson, paul.nilsson@cern.ch
+
+import unittest
+
+import pilot.util
+
+class TestUtils(unittest.TestCase):
+    """
+    Unit tests for utils functions.
+    """
+
+    def test_collect_workernode_info(self):
+        """
+        Make sure that collect_workernode_info() returns the proper types (float, float).
+
+        :return: (assertion)
+        """
+
+        mem, cpu = pilot.util.collect_workernode_info()
+
+        self.assertEqual(type(mem), float)
+        self.assertEqual(type(cpu), float)
+
