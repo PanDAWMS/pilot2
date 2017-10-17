bf7ec175bede375cbd08c5c7de422204157d1ed6
palnilsson <Paul.Nilsson@cern.ch>
2017-10-17 14:37:30 +0200

    Name refactoring

--- pilot/user/atlas/common.py
+++ pilot/user/atlas/common.py
@@ -7,5 +7,26 @@
 # Authors:
 # - Paul Nilsson, paul.nilsson@cern.ch, 2017
 
+from pilot.common.exception import PilotException
+from pilot.user.atlas.setup import should_pilot_prepare_asetup
+
 import logging
 logger = logging.getLogger(__name__)
+
+
+def get_payload_command(job):
+    """
+    Return the full command for execuring the payload, including the sourcing of all setup files and setting of
+    environment variables.
+
+    :param job: job object
+    :return: command (string)
+    """
+
+    # Should the pilot do the asetup or do the jobPars already contain the information?
+    prepareASetup = should_pilot_prepare_asetup(job.noExecStrCnv, job.jobPars)
+
+    # Is it a user job or not?
+    analysisJob = isAnalysisJob(job.trf)
+
+    return ""
--- pilot/user/atlas/setup.py
+++ pilot/user/atlas/setup.py
@@ -68,16 +68,3 @@ def is_user_analysis_job(trf):
     return analysisjob
 
 
-def get_payload_command(job):
-    """
-    Return the full command for execuring the payload, including the sourcing of all setup files and setting of
-    environment variables.
-
-    :param job: job object
-    :return: command (string)
-    """
-
-    # Should the pilot do the asetup or do the jobPars already contain the information?
-    prepareASetup = should_pilot_prepare_asetup(job.noExecStrCnv, job.jobPars)
-
-    return ""
