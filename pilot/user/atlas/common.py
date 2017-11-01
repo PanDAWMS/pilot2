6a00ae49401e077d2730c960a1446976c692513c
PalNilsson <paul.nilsson@cern.ch>
2017-11-01 16:18:26 +0100

    Initial get_payload_command()

--- pilot/user/atlas/common.py
+++ pilot/user/atlas/common.py
@@ -8,13 +8,14 @@
 # - Paul Nilsson, paul.nilsson@cern.ch, 2017
 
 # from pilot.common.exception import PilotException
-# from pilot.user.atlas.setup import should_pilot_prepare_asetup, is_user_analysis_job, get_cmtconfig
+from pilot.user.atlas.setup import should_pilot_prepare_asetup, is_user_analysis_job, get_platform, get_asetup, \
+    is_standard_atlas_job
 
 import logging
 logger = logging.getLogger(__name__)
 
 
-def get_payload_command(job, queuedata):
+def get_payload_command(job):
     """
     Return the full command for execuring the payload, including the sourcing of all setup files and setting of
     environment variables.
@@ -24,12 +25,25 @@ def get_payload_command(job, queuedata):
     """
 
     # Should the pilot do the asetup or do the jobPars already contain the information?
-    # prepareASetup = should_pilot_prepare_asetup(job.noExecStrCnv, job.jobPars)
+    prepareasetup = should_pilot_prepare_asetup(job.noExecStrCnv, job.jobPars)
 
     # Is it a user job or not?
-    # analysisJob = is_user_analysis_job(job.trf)
+    userjob = is_user_analysis_job(job.trf)
 
-    # Get the cmtconfig value
-    # cmtconfig = get_cmtconfig(job.cmtconfig, queuedata)
+    # Get the platform value
+    platform = get_platform(job.cmtConfig)
+
+    # Define the setup for asetup, i.e. including full path to asetup and setting of ATLAS_LOCAL_ROOT_BASE
+    asetuppath = get_asetup(asetup=prepareasetup)
+    asetupoptions = " "
+
+    if is_standard_atlas_job():
+
+        # Normal setup (production and user jobs)
+        logger.info("preparing normal production/analysis job setup command")
+
+    else:  # Generic, non-ATLAS specific jobs, or at least a job with undefined swRelease
+
+        logger.info("generic job (non-ATLAS specific or with undefined swRelease)")
 
     return ""
