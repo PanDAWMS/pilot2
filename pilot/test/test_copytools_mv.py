462214deada169ef7d7acfbd73c5670728e51b51
Pavlo Svirin <pavlosvirin@130-199-21-196.dhcp.bnl.gov>
2017-12-11 19:20:58 -0500

    Fixed unit test for mv copy tool

--- pilot/test/test_copytools_mv.py
+++ pilot/test/test_copytools_mv.py
@@ -4,20 +4,21 @@
 # You may obtain a copy of the License at
 # http://www.apache.org/licenses/LICENSE-2.0
 #
-# Authors:
-# - Pavlo Svirin, pavlo.svirin@cern.ch
+# Authors: Pavlo Svirin <pavlo.svirin@gmail.com>
 
 import unittest
 import string
 import tempfile
 import shutil
 import random
+import os
 import os.path
 import sys
 
 #from pilot.util.workernode import collect_workernode_info, get_disk_space_for_dispatcher
 from pilot.copytool.mv import *
 from pilot.common.exception import StageInFailure, StageOutFailure
+from pilot.util.container import execute
 
 
 class TestCopytoolMv(unittest.TestCase):
@@ -36,9 +37,6 @@ class TestCopytoolMv(unittest.TestCase):
         """ Create temp source directory """
         self.tmp_src_dir = tempfile.mkdtemp()
 
-        print("Temporary source directory: %s" % self.tmp_src_dir)
-        print("Temporary destination directory: %s" % self.tmp_dst_dir)
-
         self.filelist = []
         """ Create temp files in source dir """
         for i in range (0, self.numFiles):
@@ -47,88 +45,78 @@ class TestCopytoolMv(unittest.TestCase):
                 # generate random data and write
                 fsize = random.randint(1, self.maxFileSize)
                 data = [ random.randint(0, 255) for x in range(0,fsize) ]
-                newFile = open(os.path.join(self.tmp_src_dir, ''.join(random.choice(string.lowercase) for x in range(20))), "wb")
+                newFile = open(os.path.join(self.tmp_src_dir, fname), "wb")
                 newFile.write(str(data))
                 newFile.close()
                 # add to list
                 self.filelist.append({'name': fname, 'source': self.tmp_src_dir, 'destination': self.tmp_dst_dir})
-        #print(self.filelist)
 
     def test_copy_in_mv(self):
+        _, stdout1, stderr1 = execute(' '.join(['ls', self.tmp_src_dir]))
         copy_in(self.filelist)
-        #self.assertEqual(exit_code, 0)
         # here check files copied
+        self.assertEqual(self.__dirs_content_valid(self.tmp_src_dir, self.tmp_dst_dir, dir1_expected_content='',dir2_expected_content=stdout1), 0)
 
+    
     def test_copy_in_cp(self):
-        print "Copy in:"
-        print(self.filelist)
         copy_in(self.filelist, copy_type='cp')
-        #self.assertEqual(exit_code, 0)
-        # here check files copied
+        self.assertEqual(self.__dirs_content_equal(self.tmp_src_dir, self.tmp_dst_dir), 0)
 
     def test_copy_in_symlink(self):
         copy_in(self.filelist, copy_type='symlink')
         # here check files linked
+        self.assertEqual(self.__dirs_content_equal(self.tmp_src_dir, self.tmp_dst_dir), 0)
+        # check dst files are links
+        _, stdout, _ = execute('find %s -type l -exec echo -n l \;' % self.tmp_dst_dir)
+        self.assertEqual(stdout, ''.join('l' for i in range(self.numFiles)))
 
     def test_copy_in_invalid(self):
         self.assertRaises(StageInFailure,  copy_in, self.filelist, **{'copy_type' : ''})
         self.assertRaises(StageInFailure,  copy_in, self.filelist, **{'copy_type' : None})
-        #self.assertRaises(StageInFailure, copy_in, self.filelist, copy_type=None)
 
-    @unittest.skip("demonstrating skipping")
     def test_copy_out_mv(self):
-        pass
-
-    @unittest.skip("demonstrating skipping")
-    def test_copy_out_cp(self):
-        pass
-
-    @unittest.skip("demonstrating skipping")
-    def test_copy_out_invalid(self):
-        self.assertRaises(StageOutFailure,  copy_in, self.filelist, **{'copy_type' : ''})
-        self.assertRaises(StageOutFailure,  copy_in, self.filelist, **{'copy_type' : 'symlink'})
-        self.assertRaises(StageOutFailure,  copy_in, self.filelist, **{'copy_type' : None})
-        #copy_out(self.filelist, copy_type='symlink')
-        #copy_out(self.filelist, copy_type=None)
+        _, stdout1, stderr1 = execute(' '.join(['ls', self.tmp_src_dir]))
+        copy_out(self.filelist)
         # here check files linked
-        pass
+        self.assertEqual(self.__dirs_content_valid(self.tmp_src_dir, self.tmp_dst_dir, dir1_expected_content='',dir2_expected_content=stdout1), 0)
+
+    def test_copy_out_cp(self):
+        copy_out(self.filelist, copy_type='cp')
+        self.assertEqual(self.__dirs_content_equal(self.tmp_src_dir, self.tmp_dst_dir), 0)
+
+    def test_copy_out_invalid(self):
+        self.assertRaises(StageOutFailure,  copy_out, self.filelist, **{'copy_type' : ''})
+        self.assertRaises(StageOutFailure,  copy_out, self.filelist, **{'copy_type' : 'symlink'})
+        self.assertRaises(StageOutFailure,  copy_out, self.filelist, **{'copy_type' : None})
 
     def tearDown(self):
         """ Drop temp directories """
         shutil.rmtree(self.tmp_dst_dir)
-        print("Dropping: " + self.tmp_dst_dir)
+        #print("Dropping: " + self.tmp_dst_dir)
         shutil.rmtree(self.tmp_src_dir)
-        print("Dropping: " + self.tmp_src_dir)
+        #print("Dropping: " + self.tmp_src_dir)
+
+    def __dirs_content_equal(self, dir1, dir2):
+        if dir1=='' or dir2=='' or dir1 is None or dir2 is None:
+            return -1
+        _, stdout1, stderr1 = execute(' '.join(['ls', dir1]))
+        _, stdout2, stderr2 = execute(' '.join(['ls', dir2]))
+        if stdout1<>stdout2:
+            return -2
+        return 0
+
+    def __dirs_content_valid(self, dir1, dir2, dir1_expected_content=None, dir2_expected_content=None):
+        if dir1=='' or dir2=='' or dir1 is None or dir2 is None:
+            return -1
+        _, stdout1, stderr1 = execute(' '.join(['ls', dir1]))
+        if dir1_expected_content is not None and stdout1<>dir1_expected_content:
+            return -3
+        _, stdout2, stderr2 = execute(' '.join(['ls', dir2]))
+        if dir2_expected_content is not None and stdout2<>dir2_expected_content:
+            return -4
+        return 0
 
 if __name__ == '__main__':
     unittest.main()
 
 
-'''
-    def test_collect_workernode_info(self):
-        """
-        Make sure that collect_workernode_info() returns the proper types (float, float).
-
-        :return: (assertion)
-        """
-
-        mem, cpu = collect_workernode_info()
-
-        self.assertEqual(type(mem), float)
-        self.assertEqual(type(cpu), float)
-
-        self.assertNotEqual(mem, 0.0)
-        self.assertNotEqual(cpu, 0.0)
-
-    def test_get_disk_space_for_dispatcher(self):
-        """
-        Verify that get_disk_space_for_dispatcher() returns the proper type (int).
-
-        :return: (assertion)
-        """
-
-        queuedata = {'maxwdir': 123456789}
-        diskspace = get_disk_space_for_dispatcher(queuedata)
-
-        self.assertEqual(type(diskspace), int)
-'''
