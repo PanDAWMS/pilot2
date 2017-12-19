5b3754ce2d36410289c5171c3511da4e318d997c
PalNilsson <paul.nilsson@cern.ch>
2017-12-19 15:53:21 +0100

    Added author

--- pilot/control/data.py
+++ pilot/control/data.py
@@ -7,6 +7,7 @@
 # Authors:
 # - Mario Lassnig, mario.lassnig@cern.ch, 2016-2017
 # - Daniel Drizhuk, d.drizhuk@gmail.com, 2017
+# - Paul Nilsson, paul.nilsson@cern.ch, 2017
 
 import copy
 import Queue
