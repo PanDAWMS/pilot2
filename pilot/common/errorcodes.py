71daf1e114c74116788dbcba0f72efc8a151c11c
PalNilsson <paul.nilsson@cern.ch>
2018-01-08 11:20:47 +0100

    Added error codes and dictionary

--- pilot/common/errorcodes.py
+++ pilot/common/errorcodes.py
@@ -7,3 +7,40 @@
 # Authors:
 # - Paul Nilsson, paul.nilsson@cern.ch, 2017
 
+class ErrorCodes:
+    """
+    Pilot error codes.
+
+    Note: Error code numbering is the same as in Pilot 1 since that is expected by the PanDA server and monitor.
+    Note 2: Add error codes as they are needed in other modules. Do not import the full Pilot 1 list at once as there
+    might very well be codes that can be reassigned/removed.
+    """
+
+    # Error code constants (from Pilot 1)
+    GENERALERROR = 1008
+    NOLOCALSPACE = 1098
+    STAGEINFAILED = 1099
+    SETUPFAILURE = 1110
+    MKDIR = 1134
+    STAGEOUTFAILED = 1137
+
+    # Error code constants (new since Pilot 2)
+    NOTIMPLEMENTED = 1300
+    UNKNOWNEXCEPTION = 1301
+    CONVERSIONFAILURE = 1302
+    FILEHANDLINGFAILURE = 1303
+    MESSAGEHANDLINGFAILURE = 1304
+    PAYLOADEXECUTIONFAILURE = 1305
+
+    error_messages = {
+        GENERALERROR: "General pilot error, consult batch log",
+        NOLOCALSPACE: "Not enough local space",
+        STAGEINFAILED: "Failed to stage-in file",
+        SETUPFAILURE: "Failed during payload setup",
+        MKDIR: "Failed to create local directory",
+        STAGEOUTFAILED: "Failed to stage-out file",
+        CONVERSIONFAILURE: "Failed to convert object data",
+        FILEHANDLINGFAILURE: "Failed during file handling",
+        MESSAGEHANDLINGFAILURE: "Failed to handle message from payload",
+        PAYLOADEXECUTIONFAILURE: "Failed to execute payload",
+    }
