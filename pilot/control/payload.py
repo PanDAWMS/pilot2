acaadb2df4bbb2440402d74b1e6c1aeb30652cd2
PalNilsson <paul.nilsson@cern.ch>
2018-01-10 13:02:16 +0100

    Debugging proc

--- pilot/control/payload.py
+++ pilot/control/payload.py
@@ -133,7 +133,14 @@ def run_payload(job, out, err):
 
     # replace platform and workdir with new function get_payload_options() or someting from experiment specific code
     try:
-        proc = execute(cmd, platform=job['cmtConfig'], workdir=job['working_dir'], returnproc=True, usecontainer=True)
+        proc = subprocess.Popen(asetup + cmd,
+                                bufsize=-1,
+                                stdout=out,
+                                stderr=err,
+                                cwd=job['working_dir'],
+                                shell=True)
+
+        # proc = execute(cmd, platform=job['cmtConfig'], workdir=job['working_dir'], returnproc=True, usecontainer=True)
     except Exception as e:
         log.error('could not execute: %s' % str(e))
         return None
