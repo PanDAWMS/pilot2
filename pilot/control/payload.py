462eb29b580cc5504a539ca3ae1129a150f689ba
PalNilsson <paul.nilsson@cern.ch>
2018-02-05 10:59:51 +0100

    Now measuring execution time with set_time_consumed()

--- pilot/control/payload.py
+++ pilot/control/payload.py
@@ -91,6 +91,27 @@ def _validate_payload(job):
     return True
 
 
+def set_time_consumed(t_tuple):
+    """
+    Set the system+user time spent by the payload.
+    The cpuConsumptionTime is the system+user time while wall time is encoded in pilotTiming (third number).
+    Previously the cpuConsumptionTime was "corrected" with a scaling factor but this was deemed outdated and is now set
+    to 1.
+    The t_tuple is defined as map(lambda x, y:x-y, t1, t0), here t0 and t1 are os.times() measured before and after
+    the payload execution command.
+
+    :param t_tuple: map(lambda x, y:x-y, t1, t0)
+    :return: cpu_consumption_unit, cpu_consumption_time, cpu_conversion_factor
+    """
+
+    t_tot = reduce(lambda x, y:x+y, t_tuple[2:3])
+    cpu_conversion_factor = 1.0
+    cpu_consumption_unit = "s" # "kSI2kseconds"
+    cpu_consumption_time = int(t_tot * cpu_conversion_factor)
+
+    return cpu_consumption_unit, cpu_consumption_time, cpu_conversion_factor
+
+
 def execute_payloads(queues, traces, args):
     """
     Execute queued payloads.
@@ -127,7 +148,13 @@ def execute_payloads(queues, traces, args):
                 payload_executor = eventservice.Executor(args, job, out, err)
             else:
                 payload_executor = generic.Executor(args, job, out, err)
+
+            # run the payload and measure the execution time
+            t0 = os.times()
             exit_code = payload_executor.run()
+            t1 = os.times()
+            t = map(lambda x, y: x - y, t1, t0)
+            job['cpuConsumptionUnit'], job['cpuConsumptionTime'], job['cpuConversionFactor'] = set_time_consumed(t)
 
             log.debug('closing payload stdout/err logs')
             out.close()
