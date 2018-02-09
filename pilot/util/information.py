cdb1743e267e55c02ad8a255aea5101184aed911
palnilsson <Paul.Nilsson@cern.ch>
2018-02-09 09:54:14 +0100

    Return empty string instead of None in case field value is None in get_container_options()

--- pilot/util/information.py
+++ pilot/util/information.py
@@ -238,7 +238,10 @@ def get_container_options():
     :return: container_options field value (string).
     """
 
-    return get_field_value('container_options')
+    container_options = get_field_value('container_options')
+    if not container_options:
+        container_options = ""
+    return container_options)
 
 
 def get_container_type():
