ceda6c78ce30e56f131b6b58534a024f6d68356c
PalNilsson <paul.nilsson@cern.ch>
2018-03-06 12:14:54 +0100

    Added import for is_harvester_mode()

--- pilot.py
+++ pilot.py
@@ -23,6 +23,7 @@ from pilot.util.information import set_location
 from pilot.info import set_info
 from pilot.util.filehandling import get_pilot_work_dir, create_pilot_work_dir
 from pilot.util.config import config
+from pilot.util.harvester import is_harvester_mode
 
 VERSION = '2018-03-06.002'
 
