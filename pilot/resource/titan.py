f2149f81d87aef63e2e033fcfda9904b4e70aa85
palnilsson <Paul.Nilsson@cern.ch>
2018-03-09 14:26:55 +0100

    Added get_setup() for titan

--- pilot/resource/titan.py
+++ pilot/resource/titan.py
@@ -16,7 +16,26 @@ def get_setup(job=None):
     Return the resource specific setup.
 
     :param job: optional job object.
-    :return: setup string.
+    :return: setup commands (list).
     """
 
-    pass
+    setup_commands = ['source /lustre/atlas/proj-shared/csc108/app_dir/pilot/grid_env/external/setup.sh',
+                      'source $MODULESHOME/init/bash',
+                      'tmp_dirname=/tmp/scratch',
+                      'tmp_dirname+="/tmp"',
+                      'export TEMP=$tmp_dirname',
+                      'export TMPDIR=$TEMP',
+                      'export TMP=$TEMP',
+                      'export LD_LIBRARY_PATH=/ccs/proj/csc108/AtlasReleases/ldpatch:$LD_LIBRARY_PATH',
+                      'export ATHENA_PROC_NUMBER=16',
+                      'export G4ATLAS_SKIPFILEPEEK=1',
+                      'export PANDA_RESOURCE=\"ORNL_Titan_MCORE\"',
+                      'export ROOT_TTREECACHE_SIZE=1',
+                      'export RUCIO_APPID=\"simul\"',
+                      'export RUCIO_ACCOUNT=\"pilot\"',
+                      'export CORAL_DBLOOKUP_PATH=/ccs/proj/csc108/AtlasReleases/21.0.15/nfs_db_files',
+                      'export CORAL_AUTH_PATH=$SW_INSTALL_AREA/DBRelease/current/XMLConfig',
+                      'export DATAPATH=$SW_INSTALL_AREA/DBRelease/current:$DATAPATH',
+                      ' ']
+
+    return setup_commands
