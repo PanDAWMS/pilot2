ae4801049493d1b3c20e51de58a2a01cfc44b890
palnilsson <Paul.Nilsson@cern.ch>
2018-03-23 09:59:34 +0100

    Updated version number

--- pilot.py
+++ pilot.py
@@ -25,7 +25,7 @@ from pilot.util.filehandling import get_pilot_work_dir, create_pilot_work_dir
 from pilot.util.config import config
 from pilot.util.harvester import is_harvester_mode
 
-VERSION = '2018-03-20.002'
+VERSION = '2018-03-23.001'
 
 
 def main():
