b797a0e303d19f7f32f424358ea2cca5efc92cf0
PalNilsson <paul.nilsson@cern.ch>
2018-05-02 14:41:11 +0200

    Added import of get_analysis_trf and set_inds

--- pilot/user/atlas/common.py
+++ pilot/user/atlas/common.py
@@ -17,7 +17,8 @@ from signal import SIGTERM, SIGUSR1
 from pilot.util.constants import UTILITY_BEFORE_PAYLOAD, UTILITY_WITH_PAYLOAD, UTILITY_AFTER_PAYLOAD,\
     UTILITY_WITH_STAGEIN
 from pilot.util.container import execute
-from pilot.user.atlas.setup import should_pilot_prepare_asetup, get_asetup, get_asetup_options, is_standard_atlas_job
+from pilot.user.atlas.setup import should_pilot_prepare_asetup, get_asetup, get_asetup_options, is_standard_atlas_job,\
+    set_inds, get_analysis_trf
 from pilot.util.filehandling import remove
 from pilot.user.atlas.utilities import get_memory_monitor_setup, get_network_monitor_setup, post_memory_monitor_action,\
     get_memory_monitor_summary_filename, get_prefetcher_setup, get_benchmark_setup
