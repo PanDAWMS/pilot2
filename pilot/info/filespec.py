fd4476147cbb6584f60473625b6021367329ce48
Alexey Anisenkov <Alexey.Anisenkov@cern.ch>
2018-05-08 12:20:13 +0700

    fix comment

--- pilot/info/filespec.py
+++ pilot/info/filespec.py
@@ -58,7 +58,7 @@ class FileSpec(BaseData):
     status = None     # file transfer status value
     status_code = 0   # file transfer status code
     inputddms = []    # list of DDMEndpoint names which will be considered by default (if set) as allowed storage for input replicas
-    workdir = None    # used to declare file-specific work dir (location on give file while it's used for transfer by copytool)
+    workdir = None    # used to declare file-specific work dir (location of given local file when it's used for transfer by copytool)
 
     # specify the type of attributes for proper data validation and casting
     _keys = {int: ['filesize', 'mtime', 'status_code'],
