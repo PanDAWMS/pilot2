3c604d796d31f25e52e9a27c688ae20cf40c6803
PalNilsson <paul.nilsson@cern.ch>
2018-05-25 15:47:34 +0200

    Updated build number

--- pilot.py
+++ pilot.py
@@ -26,7 +26,7 @@ from pilot.util.config import config
 from pilot.util.harvester import is_harvester_mode
 from pilot.util.node import is_virtual_machine
 
-VERSION = '2018-05-25.001'
+VERSION = '2018-05-25.002'
 
 
 def pilot_version_banner():
