49352f8ec7ba00c2895e67996d311a0399173e86
PalNilsson <paul.nilsson@cern.ch>
2018-06-05 17:04:14 +0200

    Created get_core_count()

--- pilot/user/atlas/jobmetrics.py
+++ pilot/user/atlas/jobmetrics.py
@@ -7,10 +7,9 @@
 # Authors:
 # - Paul Nilsson, paul.nilsson@cern.ch, 2018
 
-import os
-
 from pilot.util.auxiliary import get_logger
 from pilot.util.jobmetrics import get_job_metrics_entry
+from pilot.util.processes import get_core_count
 
 import logging
 
@@ -35,23 +34,7 @@ def get_job_metrics(job):
 
     job_metrics = ""
 
-    if "HPC_HPC" in job.infosys.queuedata.catchall:
-        if job.corecount is None:
-            job.corecount = 0
-    else:
-        if job.corecount:
-            # Always use the ATHENA_PROC_NUMBER first, if set
-            if 'ATHENA_PROC_NUMBER' in os.environ:
-                try:
-                    job.corecount = int(os.environ.get('ATHENA_PROC_NUMBER'))
-                except Exception as e:
-                    log("ATHENA_PROC_NUMBER is not properly set: %s (will use existing job.corecount value)" % e)
-        else:
-            try:
-                job.corecount = int(os.environ.get('ATHENA_PROC_NUMBER'))
-            except Exception:
-                log("environment variable ATHENA_PROC_NUMBER is not set. corecount is not set")
-    corecount = job.corecount
+    corecount = get_core_count(job)
 
     if corecount is not None and corecount != "NULL" and corecount != 'null':
         job_metrics += get_job_metrics_entry(key="coreCount", value=corecount)
