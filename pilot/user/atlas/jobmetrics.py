5b22ca96b14a72c5a41ab0d2d335a8fb43f8c230
PalNilsson <paul.nilsson@cern.ch>
2018-06-05 16:56:25 +0200

    Added coreCount to job metrics

--- pilot/user/atlas/jobmetrics.py
+++ pilot/user/atlas/jobmetrics.py
@@ -7,10 +7,13 @@
 # Authors:
 # - Paul Nilsson, paul.nilsson@cern.ch, 2018
 
+import os
+
 from pilot.util.auxiliary import get_logger
 from pilot.util.jobmetrics import get_job_metrics_entry
 
 import logging
+
 logger = logging.getLogger(__name__)
 
 
@@ -28,25 +31,29 @@ def get_job_metrics(job):
     :return: job metrics (string).
     """
 
-    if "HPC_HPC" in readpar('catchall'):
-        if job.coreCount is None:
-            job.coreCount = 0
+    log = get_logger(job.jobid)
+
+    job_metrics = ""
+
+    if "HPC_HPC" in job.infosys.queuedata.catchall:
+        if job.corecount is None:
+            job.corecount = 0
     else:
-        if job.coreCount:
+        if job.corecount:
             # Always use the ATHENA_PROC_NUMBER first, if set
-            if os.environ.has_key('ATHENA_PROC_NUMBER'):
+            if 'ATHENA_PROC_NUMBER' in os.environ:
                 try:
-                    job.coreCount = int(os.environ['ATHENA_PROC_NUMBER'])
-                except Exception, e:
-                    tolog("ATHENA_PROC_NUMBER is not properly set: %s (will use existing job.coreCount value)" % (e))
+                    job.corecount = int(os.environ.get('ATHENA_PROC_NUMBER'))
+                except Exception as e:
+                    log("ATHENA_PROC_NUMBER is not properly set: %s (will use existing job.corecount value)" % e)
         else:
             try:
-                job.coreCount = int(os.environ['ATHENA_PROC_NUMBER'])
-            except:
-                tolog("env ATHENA_PROC_NUMBER is not set. corecount is not set")
-    coreCount = job.coreCount
-    jobMetrics = ""
-    if coreCount is not None and coreCount != "NULL" and coreCount != 'null':
-        jobMetrics += self.jobMetric(key="coreCount", value=coreCount)
+                job.corecount = int(os.environ.get('ATHENA_PROC_NUMBER'))
+            except Exception:
+                log("environment variable ATHENA_PROC_NUMBER is not set. corecount is not set")
+    corecount = job.corecount
+
+    if corecount is not None and corecount != "NULL" and corecount != 'null':
+        job_metrics += get_job_metrics_entry(key="coreCount", value=corecount)
 
     return job_metrics
