1c1fd2a62975f77c735f596bd5fe346cf418f9c0
palnilsson <Paul.Nilsson@cern.ch>
2018-06-08 16:52:12 +0200

    Out-commented import of get_logger for now

--- pilot/user/atlas/jobmetrics.py
+++ pilot/user/atlas/jobmetrics.py
@@ -7,7 +7,7 @@
 # Authors:
 # - Paul Nilsson, paul.nilsson@cern.ch, 2018
 
-from pilot.util.auxiliary import get_logger
+# from pilot.util.auxiliary import get_logger
 from pilot.util.jobmetrics import get_job_metrics_entry
 from pilot.util.processes import get_core_count
 
