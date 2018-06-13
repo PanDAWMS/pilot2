8d10f601b12133b439fcc043aa60a7ef4e94e079
PalNilsson <atlas-adc-pilot@cern.ch>
2018-06-13 12:05:47 +0200

    Initial implementation for Analytics() class

--- pilot/api/analytics.py
+++ pilot/api/analytics.py
@@ -18,6 +18,72 @@ class Analytics(Services):
     Analytics service class.
     """
 
+    _fit = None
+
+    def __init__(self, *args):
+        """
+        Init function.
+
+        :param args:
+        """
+
+        self._fit = None
+
+        pass
+
+    def fit(self, x, y, model='linear'):
+        """
+        Fitting function.
+
+        :param x: list of input data (list of floats or ints).
+        :param y: list of input data (list of floats or ints).
+        :param model: model name (string).
+        :return:
+        """
+
+        self._fit = Fit()
+
+        return self._fit
+
+    def chi2(self):
+        """
+        Return the Chi2 of the fit.
+
+        :return: chi2 (float).
+        """
+
+        # calculate Chi2
+        # ..
+        return 0.0
+
+    def slope(self):
+        """
+        Calculate the slope of a linear fit.
+
+        :return: slope (float).
+        """
+
+        # calculate slope
+        # ..
+        return 0.0
+
+    def intersect(self):
+        """
+        Calculate the intersect of a linear fit.
+
+        :return: intersect (float).
+        """
+
+        # calculate slope
+        # ..
+        return 0.0
+
+
+class Fit(object):
+    """
+    Low-level fitting class.
+    """
+
     def __init__(self, *args):
         """
         Init function.
