511a741fac7ec2e6ed668443558f7e7571599e6c
PalNilsson <paul.nilsson@cern.ch>
2018-06-13 16:37:26 +0200

    Implemented mean(), sum_square_dev(), sum_dev()

--- pilot/util/math.py
+++ pilot/util/math.py
@@ -12,3 +12,49 @@
 import logging
 
 logger = logging.getLogger(__name__)
+
+
+def mean(data):
+    """
+    Return the sample arithmetic mean of data.
+
+    :param data: list of floats or ints.
+    :return: mean value (float).
+    """
+
+    n = len(data)
+    if n < 1:
+        raise ValueError('mean requires at least one data point')
+
+    # return sum(data)/n # in Python 2 use sum(data)/float(n)
+    return sum(data)/float(n)
+
+
+def sum_square_dev(data):
+    """
+    Return sum of square deviations of sequence data.
+    Sum (x - x_mean)**2
+
+    :param data: list of floats or ints.
+    :return: sum of squares (float).
+    """
+
+    c = mean(data)
+
+    return sum((x - c) ** 2 for x in data)
+
+
+def sum_dev(x, y):
+    """
+    Return sum of deviations of sequence data.
+    Sum (x - x_mean)**(y - y_mean)
+
+    :param x: list of ints or floats.
+    :param y:  list of ints or floats.
+    :return: sum of deviations (float).
+    """
+
+    c1 = mean(x)
+    c2 = mean(y)
+
+    return sum((_x - c1) * (_y - c2) for _x, _y in zip(x, y))
