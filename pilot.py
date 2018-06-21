a7429738f20654ac9b5923baf48c5830c2b05897
PalNilsson <paul.nilsson@cern.ch>
2018-06-21 14:42:20 +0200

    Updated pilot version

--- pilot.py
+++ pilot.py
@@ -27,7 +27,7 @@ from pilot.util.information import set_location
 from pilot.util.workernode import is_virtual_machine
 from pilot.util.timing import add_to_pilot_timing
 
-VERSION = '2018-06-20.001'
+VERSION = '2018-06-21.001'
 
 
 def pilot_version_banner():
