2e56adb2e05ff63f699e7754ae81010e71d84be8
PalNilsson <paul.nilsson@cern.ch>
2018-07-11 15:08:42 +0200

    Corrected string usage in verify_running_processes()

--- pilot.py
+++ pilot.py
@@ -204,9 +204,15 @@ if __name__ == '__main__':
 
     arg_parser.add_argument('-z',
                             dest='update_server',
-                            action='store_false',
                             default=True,
-                            help='Disable server updates')
+                            type=bool,
+                            help='Update server (default: True)')
+
+#    arg_parser.add_argument('-z',
+#                            dest='update_server',
+#                            action='store_false',
+#                            default=True,
+#                            help='Disable server updates')
 
     arg_parser.add_argument('-t',
                             dest='verify_proxy',
