987c2076e9dc931a831bc6ee7936fed885880b88
PalNilsson <paul.nilsson@cern.ch>
2018-10-19 10:54:07 +0200

    Updated release number comments

--- pilot.py
+++ pilot.py
@@ -31,10 +31,10 @@ from pilot.util.mpi import get_ranks_info
 from pilot.util.timing import add_to_pilot_timing
 from pilot.util.workernode import is_virtual_machine
 
-RELEASE = '2'   # fixed at 2 for Pilot 2
-VERSION = '0'   # '1' for first real Pilot 2 release, '0' until then, increased for bigger updates
-REVISION = '0'  # reset to '0' for every new Pilot version release, increased for small updates
-BUILD = '129'   # reset to '1' for every new development cycle
+RELEASE = '2'   # released number should be fixed at 2 for Pilot 2
+VERSION = '0'   # version number is '1' for first real Pilot 2 release, '0' until then, increased for bigger updates
+REVISION = '0'  # revision number should be reset to '0' for every new version release, increased for small updates
+BUILD = '130'   # build number should be reset to '1' for every new development cycle
 
 
 def pilot_version_banner():
