ec2c555b9e9089c66677a6d6db8c0b25e9f2327a
PalNilsson <atlas-adc-pilot@cern.ch>
2018-10-19 11:30:33 +0200

    Added error code 1220, UNKNOWNPAYLOADFAILURE: "Job failed due to unknown reason (consult log file)"

--- pilot/common/errorcodes.py
+++ pilot/common/errorcodes.py
@@ -62,6 +62,7 @@ class ErrorCodes:
     MISSINGINSTALLATION = 1211
     PAYLOADOUTOFMEMORY = 1212
     REACHEDMAXTIME = 1213
+    UNKNOWNPAYLOADFAILURE = 1220
     FILEEXITS = 1221
     BADALLOC = 1223
     ESRECOVERABLE = 1224
@@ -138,6 +139,7 @@ class ErrorCodes:
         MISSINGINSTALLATION: "Missing installation",
         PAYLOADOUTOFMEMORY: "Payload ran out of memory",
         REACHEDMAXTIME: "Reached batch system time limit",
+        UNKNOWNPAYLOADFAILURE: "Job failed due to unknown reason (consult log file)",
         FILEEXITS: "File already exists",
         BADALLOC: "Transform failed due to bad_alloc",
         CHKSUMNOTSUP: "Query checksum is not supported",
