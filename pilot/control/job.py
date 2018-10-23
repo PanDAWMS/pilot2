1bf4ca2126c5bfb412f485f729979e528fea4142
PalNilsson <paul.nilsson@cern.ch>
2018-10-23 16:02:26 +0200

    Now reading JSON dictionary from Harvester job def file

--- pilot/control/job.py
+++ pilot/control/job.py
@@ -32,7 +32,7 @@ from pilot.util.config import config
 from pilot.util.common import should_abort
 from pilot.util.constants import PILOT_PRE_GETJOB, PILOT_POST_GETJOB, PILOT_KILL_SIGNAL, LOG_TRANSFER_NOT_DONE, \
     LOG_TRANSFER_IN_PROGRESS, LOG_TRANSFER_DONE, LOG_TRANSFER_FAILED
-from pilot.util.filehandling import get_files, tail
+from pilot.util.filehandling import get_files, tail, read_json
 from pilot.util.harvester import request_new_jobs, remove_job_request_file
 from pilot.util.jobmetrics import get_job_metrics
 from pilot.util.monitoring import job_monitor_tasks, check_local_space
@@ -573,10 +573,12 @@ def get_job_definition_from_file(path, harvester):
     :return: job definition dictionary.
     """
 
-    # remove any existing Harvester job request files (silent in non-Harvester mode)
+    # remove any existing Harvester job request files (silent in non-Harvester mode) and read the JSON
     if harvester:
         remove_job_request_file()
+        return read_json(path)
 
+    # old style
     res = {}
     with open(path, 'r') as jobdatafile:
         response = jobdatafile.read()
