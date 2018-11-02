5f4762776715ac26b4dfede68100631e8900d17c
palnilsson <Paul.Nilsson@cern.ch>
2018-11-02 16:18:56 +0100

    Removed test code

--- pilot/user/atlas/setup.py
+++ pilot/user/atlas/setup.py
@@ -107,8 +107,6 @@ def get_asetup(asetup=True, alrb=False):
     :return: source <path>/asetup.sh (string).
     """
 
-    #raise NoSoftwareDir('testing')
-
     cmd = ""
     alrb_cmd = get_alrb_export()
     if alrb_cmd != "":
