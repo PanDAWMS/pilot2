2c2168ea208795acbbd787dc5cf51e25796a013a
Paul Nilsson <paul.nilsson@cern.ch>
2018-11-19 16:38:04 +0100

    Correction

--- pilot/user/atlas/setup.py
+++ pilot/user/atlas/setup.py
@@ -372,7 +372,7 @@ def get_payload_environment_variables(cmd, job_id, task_id, processing_type, sit
         log.warning("RUCIO_APPID needs job.processingType but it is not set!")
     else:
         variables.append('export RUCIO_APPID=%s;' % processing_type)
-    variables.append('export RUCIO_ACCOUNT=%";' % os.environ.get('RUCIO_ACCOUNT', 'pilot'))
+    variables.append('export RUCIO_ACCOUNT=%s;' % os.environ.get('RUCIO_ACCOUNT', 'pilot'))
 
     return variables
 
