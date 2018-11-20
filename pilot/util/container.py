ba562034e37ae73c3714245c400885a87e5f6330
palnilsson <Paul.Nilsson@cern.ch>
2018-11-20 11:08:01 +0100

    Now quoting parantheses for containers

--- pilot/util/container.py
+++ pilot/util/container.py
@@ -70,8 +70,10 @@ def execute(executable, **kwargs):
             logger.warning('container module could not be imported')
 
     if not mute:
-        #if job and job.is_analysis() and job.imagename == "" and usecontainer:
-        #    # filter the command
+        if job and job.is_analysis() and job.imagename == "" and usecontainer:
+            # filter the command
+            executable = executable.replace("(", "\(")
+            executable = executable.replace(")", "\)")
         #    executable = executable.replace("\'", "\\'")
         #    executable = executable.replace('\"', '\\"')
         #    logger.info('(filtered command)')
