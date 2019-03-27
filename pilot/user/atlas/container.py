158a1932af914a5db5371ca83ffca0083adaf88b
Paul Nilsson <paul.nilsson@cern.ch>
2019-03-27 15:33:30 +0100

    Update for job.alrbuserplatform

--- pilot/user/atlas/container.py
+++ pilot/user/atlas/container.py
@@ -211,7 +211,9 @@ def alrb_wrapper(cmd, workdir, job):
             "resolved singularity_options from queuedata.container_options: %s" % singularity_options)
 
         _cmd = asetup
-        if job.platform and not job.alrbuserplatform and '--containerImage' not in job.jobparams:
+        if job.alrbuserplatform:
+            _cmd += 'export thePlatform=\"%s\";' % job.alrbuserplatform
+        elif job.platform and not job.alrbuserplatform and '--containerImage' not in job.jobparams:
             _cmd += 'export thePlatform=\"%s\";' % job.platform
         #elif '--containerImage' in job.jobparams:
         #    if job.alrbuserplatform:
