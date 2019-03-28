2b439a99ef3aff330488365bacd00d56c651856e
Paul Nilsson <paul.nilsson@cern.ch>
2019-03-28 13:48:04 +0100

    Updated looping job killer with last modification time of found file instead of using system time at the time of measurement (which is not correct)

--- pilot/util/loopingjob.py
+++ pilot/util/loopingjob.py
@@ -102,12 +102,15 @@ def get_time_for_last_touch(job, mt, looping_limit):
 
             # remove unwanted list items (*.py, *.pyc, workdir, ...)
             files = loopingjob_definitions.remove_unwanted_files(job.workdir, files)
-            if files != []:
-                log.info('found %d files that were recently updated (e.g. file \"%s\"' % (len(files), files[0]))
-                log.info(str(files))
+            if files:
+                log.info('found %d files that were recently updated' % len(files))
 
-                # update the current system time
-                mt.update('ct_looping_last_touched')
+                # now get the mod times for these file, and identify the most recently update file
+                latest_modified_file, mtime = find_latest_modified_file(files)
+                log.info("File %s is the most recently updated file (at time=%d)" % (latest_modified_file, mtime))
+
+                # store the time of the last file modification
+                mt.update('ct_looping_last_touched', modtime=mtime)
             else:
                 log.warning("found no recently updated files!")
         else:
