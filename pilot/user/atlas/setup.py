c3c1f65391def8a784782dae3e7ba6a211e30ef7
PalNilsson <atlas-adc-pilot@cern.ch>
2019-04-03 12:09:01 +0200

    Now appending export of PandaID and PanDA_TaskID to payload setup command, otherwise these values get lost inside containers, as reported by EventIndex people

--- pilot/user/atlas/setup.py
+++ pilot/user/atlas/setup.py
@@ -357,6 +357,8 @@ def get_payload_environment_variables(cmd, job_id, task_id, processing_type, sit
     variables.append('export PANDA_RESOURCE=\'%s\';' % site_name)
     variables.append('export FRONTIER_ID=\"[%s_%s]\";' % (task_id, job_id))
     variables.append('export CMSSW_VERSION=$FRONTIER_ID;')
+    variables.append('export PandaID=%s' % os.environ.get('PandaID', 'unknown'))
+    variables.append('export PanDA_TaskID=%s' % os.environ.get('PanDA_TaskID', 'unknown'))
 
     # Unset ATHENA_PROC_NUMBER if set for event service Merge jobs
     if "Merge_tf" in cmd and 'ATHENA_PROC_NUMBER' in os.environ:
