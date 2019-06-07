6e4187dfd46d7d2899d9838d7edf08b59c5ccc66
Paul Nilsson <paul.nilsson@cern.ch>
2019-06-07 14:22:27 +0200

    Added workflow choices stagein and payload_stageout

--- pilot.py
+++ pilot.py
@@ -209,7 +209,7 @@ def get_args():
                             choices=['generic', 'generic_hpc',
                                      'production', 'production_hpc',
                                      'analysis', 'analysis_hpc',
-                                     'eventservice_hpc'],
+                                     'eventservice_hpc', 'stagein', 'payload_stageout'],
                             help='Pilot workflow (default: generic)')
 
     # graciously stop pilot process after hard limit
