da873695f257b548c018a2880211660c037d27a4
Paul Nilsson <paul.nilsson@cern.ch>
2019-07-11 15:40:03 +0200

    Flake8 correction

--- pilot/user/atlas/common.py
+++ pilot/user/atlas/common.py
@@ -1261,7 +1261,7 @@ def get_utility_command_setup(name, job, setup=None):
 
     if name == 'MemoryMonitor':
         setup, pid = get_memory_monitor_setup(job.pid, job.workdir, job.command, use_container=job.usecontainer,
-                                         transformation=job.transformation, outdata=job.outdata)
+                                              transformation=job.transformation, outdata=job.outdata)
         _pattern = r"([\S]+)\ ."
         pattern = re.compile(_pattern)
         _name = re.findall(pattern, setup.split(';')[-1])
