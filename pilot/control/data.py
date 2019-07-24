b67aa0e581ab9ade8c9585d175354d19b9e7d65c
Paul Nilsson <paul.nilsson@cern.ch>
2019-07-24 14:34:51 +0200

    Using lfn instead of pfn in PoolFileCatalog since some trf's have problems with full paths

--- pilot/control/data.py
+++ pilot/control/data.py
@@ -504,7 +504,7 @@ def copytool_out(queues, traces, args):
     logger.debug('[data] copytool_out thread has finished')
 
 
-def get_input_file_dictionary(indata, workdir):
+def get_input_file_dictionary(indata, workdir, lfn=True):
     """
     Return an input file dictionary.
     Format: {'guid': 'pfn', ..}
@@ -521,8 +521,10 @@ def get_input_file_dictionary(indata, workdir):
         dst = e.workdir or workdir or '.'
         file_dictionary[e.guid] = e.turl if e.accessmode == 'direct' else os.path.join(dst, e.lfn)
         # file_dictionary[e.guid] = e.turl if e.accessmode == 'direct' else e.surl
+
         # correction for ND and mv
-        if not file_dictionary[e.guid]:
+        # in any case use the lfn instead of pfn since there are trf's that have problems with pfn's
+        if not file_dictionary[e.guid] or lfn:
             file_dictionary[e.guid] = e.lfn
 
     return file_dictionary
