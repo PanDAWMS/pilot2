8092b08200240899d81af4216ad37845db8c56cb
Alexey Anisenkov <Alexey.Anisenkov@cern.ch>
2019-07-30 15:59:01 +0200

    `TimedProcess` bugfix: properly propagate exception in case of function failure (current implementation does not preserve original stack trace on exception: to be fixed)

--- pilot/util/timer.py
+++ pilot/util/timer.py
@@ -140,7 +140,7 @@ class TimedProcess(object):
         if ret[0]:
             return ret[1]
         else:
-            raise ret[1][0], ret[1][1], ret[1][2]
+            raise ret[1]
 
 
 if getattr(os, 'fork', None):
