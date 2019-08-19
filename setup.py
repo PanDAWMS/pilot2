27c005c094e9bfff4fb31aa4389f213f327877e1
fbarreir <fh.barreiro@gmail.com>
2019-08-19 20:07:23 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#
+#
+# Setup for Pilot 2
+#
+#
+import sys
+
+from setuptools import setup, find_packages
+
+sys.path.insert(0, '.')
+
+# get release version
+with open('PILOTVERSION') as reader:
+    release_version = reader.read()
+    print release_version
+
+setup(
+    name="pilot",
+    version=release_version,
+    description='PanDA Pilot 2',
+    long_description='''This package contains the PanDA pilot 2 code''',
+    license='Apache License 2.0',
+    author='Panda Team',
+    author_email='atlas-adc-panda@cern.ch',
+    url='https://github.com/PanDAWMS/pilot2/wiki',
+    python_requires='>=2.7',
+    packages=find_packages(),
+    install_requires=[], # TODO: check with Paul the dependencies
+    data_files=[], # TODO: check with Paul if anything apart of the code needs to be installed
+    scripts=[]
+    )
