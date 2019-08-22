ea44854544e9346f651b5c64248919be0b35e400
Paul Nilsson <paul.nilsson@cern.ch>
2019-08-22 18:15:49 +0200

    Update

--- pilot/user/atlas/common.py
+++ pilot/user/atlas/common.py
@@ -828,6 +828,9 @@ def verify_output_files(job):  # noqa: C901
                     job.piloterrorcodes, job.piloterrordiags = errors.add_error_code(errors.EMPTYOUTPUTFILE)
                     failed = True
                     break
+                elif nentries is None and lfn in job.allownooutput:
+                    log.warning('output file %s is listed in job report, nentries is None and is listed in allowNoOutput - remove from stage-out' % lfn)
+                    remove_from_stageout(lfn, job)
                 elif nentries is not None and nentries == 0 and lfn not in job.allownooutput:
                     log.warning('output file %s is listed in job report, has zero events and is not listed in allowNoOutput - job will fail' % lfn)
                     job.piloterrorcodes, job.piloterrordiags = errors.add_error_code(errors.EMPTYOUTPUTFILE)
