ade84345ecd49aaa1083c9766df635cdde44f32e
Paul Nilsson <paul.nilsson@cern.ch>
2020-01-06 15:25:30 +0100

    Flake8 corrections

--- pilot/control/data.py
+++ pilot/control/data.py
@@ -35,7 +35,7 @@ from pilot.util.config import config
 from pilot.util.constants import PILOT_PRE_STAGEIN, PILOT_POST_STAGEIN, PILOT_PRE_STAGEOUT, PILOT_POST_STAGEOUT,\
     LOG_TRANSFER_IN_PROGRESS, LOG_TRANSFER_DONE, LOG_TRANSFER_NOT_DONE, LOG_TRANSFER_FAILED, SERVER_UPDATE_RUNNING, MAX_KILL_WAIT_TIME
 from pilot.util.container import execute
-from pilot.util.filehandling import find_executable, remove, write_json, copy
+from pilot.util.filehandling import find_executable, remove  #, write_json, copy
 from pilot.util.timing import add_to_pilot_timing
 from pilot.util.tracereport import TraceReport
 from pilot.util.queuehandling import declare_failed_by_kill, put_in_queue
