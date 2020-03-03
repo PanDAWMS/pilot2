c19fbdaff992ec428bda5a9cb794881e7a056d90
PalNilsson <paul.nilsson@cern.ch>
2020-03-03 11:12:24 +0100

    Corrected usage of get_list_of_log_files(); problem spotted by Ivan in debug mode

--- pilot/control/job.py
+++ pilot/control/job.py
@@ -643,7 +643,7 @@ def get_payload_log_tail(job):
     stdout_tail = ""
 
     # find the latest updated log file
-    list_of_files = get_list_of_log_files(job)
+    list_of_files = get_list_of_log_files()
     if not list_of_files:
         log.info('no log files were found (will use default %s)' % config.Payload.payloadstdout)
         list_of_files = [os.path.join(job.workdir, config.Payload.payloadstdout)]  # get_files(pattern=config.Payload.payloadstdout)
