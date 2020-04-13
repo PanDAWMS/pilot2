efb5909316373fb2d6a055bdf0abc19b28f8e3b1
Paul Nilsson <paul.nilsson@cern.ch>
2020-04-13 11:34:50 +0200

    Removed the 'if [ -z $ATLAS_LOCAL_ROOT_BASE ]; then ' line again

--- pilot/user/atlas/container.py
+++ pilot/user/atlas/container.py
@@ -255,9 +255,9 @@ def update_alrb_setup(cmd, new_mode):
     try:
         _cmd = cmd.split(';')
         for subcmd in _cmd:
-            if subcmd.startswith('export ATLAS_LOCAL_ROOT_BASE'):
-                updated_cmds.append('if [ -z $ATLAS_LOCAL_ROOT_BASE ]; then ' + subcmd + '; fi')
-            elif subcmd.startswith('source ${ATLAS_LOCAL_ROOT_BASE}') and new_mode:
+            #if subcmd.startswith('export ATLAS_LOCAL_ROOT_BASE'):
+            #    updated_cmds.append('if [ -z $ATLAS_LOCAL_ROOT_BASE ]; then ' + subcmd + '; fi')
+            if subcmd.startswith('source ${ATLAS_LOCAL_ROOT_BASE}') and new_mode:
                 updated_cmds.append('export ALRB_CONT_SETUPFILE="/srv/%s"' % config.Container.release_setup)
                 updated_cmds.append(subcmd)
             else:
