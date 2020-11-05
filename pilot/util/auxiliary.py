0d87891edf78775bfd1f6aab156f037c9616cc2f
Paul Nilsson <palnilsson70@gmail.com>
2020-11-05 11:23:55 +0100

    Added get_size() to measure object sizes

--- pilot/util/auxiliary.py
+++ pilot/util/auxiliary.py
@@ -398,3 +398,31 @@ def get_resource_name():
     if not resource_name:
         resource_name = 'grid'
     return resource_name
+
+
+def get_size(obj, seen=None):
+    """
+    Recursively find the size of any objects
+
+    :param obj: object.
+    """
+
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum([get_size(v, seen) for v in obj.values()])
+        size += sum([get_size(k, seen) for k in obj.keys()])
+    elif hasattr(obj, '__dict__'):
+        size += get_size(obj.__dict__, seen)
+    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum([get_size(i, seen) for i in obj])
+
+    return size
