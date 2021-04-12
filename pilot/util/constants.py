1e8c36e5d7b50ebc1460b302f64630898104daf5
Paul Nilsson <paul.nilsson@cern.ch>
2021-04-12 12:45:38 +0200

    New major version number - 2.10.3 -> 2.11.1 due to many changes

--- pilot/util/constants.py
+++ pilot/util/constants.py
@@ -12,8 +12,8 @@ from os import environ
 
 # Pilot version
 RELEASE = '2'   # released number should be fixed at 2 for Pilot 2
-VERSION = '10'   # version number is '1' for first real Pilot 2 release, '0' until then, increased for bigger updates
-REVISION = '3'  # revision number should be reset to '0' for every new version release, increased for small updates
+VERSION = '11'   # version number is '1' for first real Pilot 2 release, '0' until then, increased for bigger updates
+REVISION = '1'  # revision number should be reset to '0' for every new version release, increased for small updates
 BUILD = '39'    # build number should be reset to '1' for every new development cycle
 
 SUCCESS = 0
