8669f8d93bcfada2f707256feb3c1c08c7bc5cf0
Paul Nilsson <palnilsson70@gmail.com>
2021-05-25 13:06:15 +0200

    Update

--- pilot/api/dask.py
+++ pilot/api/dask.py
@@ -72,7 +72,7 @@ class Dask(object):
         for cmd in commands:
             exit_code, stdout, stderr = execute('which %s' % cmd, mute=True)
             found = True if not 'not found' in stdout else False
-            if not found in stdout:
+            if found not in stdout:
                 logger.warning(stdout)
                 break
         if not found:
